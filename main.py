39c7bc535af88a4aaa266667a85a5039211eba8d
mael boyer <mael34172@gmail.com>
2024-01-29 17:40:39 +0100

    QUOICOUBEH TA LES CRAMPTE ? HEIN APAGNAN

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import interface_graphique
 
-user = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8"]
+users = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8"]
+
+
+def update_users():
+    global users
+    app.radiobutton_frame.update(users)
+    app.after(1000, update_users)
+
 
 if __name__ == '__main__':
-    app = interface_graphique.App()
+    app = interface_graphique.App(fg_color=None, text_color=None, border_color=None, border_width=5,
+                                  corner_radius=6)
     app.title("Discord mais en moin bien")
-    app.radiobutton_frame.update(user)
+    update_users()
     app.textbox_frame.print("Hello world! \n" * 100)
+
     app.mainloop()
