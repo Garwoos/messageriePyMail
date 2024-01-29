6d7a85e3545dcdb5a812d3d5273134f11267a76c
mael boyer <mael34172@gmail.com>
2024-01-29 13:34:22 +0100

    j'ai refait l'infrastructure du code, j'ai deplacé le code de main.py vers server.py, j'ai changer la library utilisé par l'interface graphique de TKinter à CustomTKinter, et j'ai fait un fichier tranformé le fichier main.py en fichier d'excution

--- interface_graphique.py
+++ interface_graphique.py
@@ -1,75 +1,68 @@
-import tkinter
+import customtkinter
 
-# Création de la fenêtre principale
-class interface(tkinter.Tk):
-    def __init__(self, window):
-        self.window = window
-        self.window.title("Interface graphique")
-        self.window.geometry("1080x720")
-        self.window.resizable(False, False)
-        
-        # Création de font de la fenêtre
-        self.canvas_output, self.inner_frame = self.new_canvas(self.window)
-         
-        for i in range(100):
-            self.display_text(self.canvas_output, f"Hello World {i}", 10, 10+(i*20))
-            
-        
-        
-    def new_canvas(self, surface, width=1060, height=650, backgroundcolor="white", pady=5):
-        """
-        Création du canvas de la fenêtre
-        """
-        frame = tkinter.Frame(surface)
-        frame.pack(fill="both")
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-        canvas = tkinter.Canvas(frame, width=width, height=height, bg=backgroundcolor)
-        canvas.pack(side="left", fill="both", pady=pady, expand=True)
+        self.title("my app")
+        self.geometry("1600x900")
+        self.minsize(400, 220)
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        scrollbar = tkinter.Scrollbar(frame, orient="vertical", command=canvas.yview)
-        scrollbar.pack(side="right", fill="y")
+        self.radiobutton_frame = MyRadiobuttonFrame(self, "Message", values=["option 1", "option 2"])
+        self.radiobutton_frame.grid(row=0, column=0, padx=(0, 10), pady=(10, 0), sticky="nsw")
 
-        canvas.configure(yscrollcommand=scrollbar.set)
+    def button_callback(self):
+        print("checkbox_frame:", self.checkbox_frame.get())
+        print("radiobutton_frame:", self.radiobutton_frame.get())
 
-        # Création d'une frame interne dans le canvas
-        inner_frame = tkinter.Frame(canvas)
-        canvas.create_window((0, 0), window=inner_frame, anchor="nw")
+class MyRadiobuttonFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.radiobuttons = []
+        self.variable = customtkinter.StringVar(value="")
 
-        # Mise à jour de la taille de la zone de défilement
-        inner_frame.bind("<Configure>", lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
 
-        return canvas, inner_frame
+        for i, value in enumerate(self.values):
+            radiobutton = customtkinter.CTkRadioButton(self, text=value, value=value, variable=self.variable)
+            radiobutton.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="nsew")
+            self.radiobuttons.append(radiobutton)
 
-    def display_text(self, surface, text, x, y, font="Arial 12", color="black"):
-        """
-        Affichage du texte sur le canvas
-        """
-        tkinter.Label(surface, text=text, font=font, bg="white", fg=color, anchor="nw").pack()
-    
-    def scroll_up(self):
-        """
-        Faire défiler le contenu du canvas vers le haut
-        """
-        self.canvas_output.yview_scroll(-1, "units")
-        
-    def scroll_down(self):
-        """
-        Faire défiler le contenu du canvas vers le bas
-        """
-        self.canvas_output.yview_scroll(1, "units")
-    
-    def on_mouse_scroll(self, event):
-        # On Windows, `event.delta` is positive for scroll up, negative for scroll down.
-        # On MacOS, `event.delta` is negative for scroll up, positive for scroll down.
-        # On Linux, `event.num` is 4 for scroll up, 5 for scroll down.
-        if event.delta > 0 or event.num == 4:
-            self.scroll_up()
-        else:
-            self.scroll_down()
-    
-    
-        
+    def get(self):
+        return self.variable.get()
+
+    def set(self, value):
+        self.variable.set(value)
+
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.checkboxes = []
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        for i, value in enumerate(self.values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i+1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
 if __name__ == "__main__":
-    window = tkinter.Tk()
-    interface(window)
-    window.mainloop()
+    app = App()
+    app.mainloop()
